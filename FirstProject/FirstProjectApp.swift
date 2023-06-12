a6add6c969f5411ffdffa20955b75ebf25cb3d92
Max Peerutin <peerutinm@gmail.com>
2023-06-12 17:53:48 +0200

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Max Peerutin on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
